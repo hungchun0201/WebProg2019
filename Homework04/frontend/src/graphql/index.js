e97884e5b4264dc5ed2f82b2bb4a160a3d9d991e
hungchun0201 <penguin7296@gmail.com>
2019-06-06 06:13:44 +0800

    delete

--- Homework04/frontend/src/graphql/index.js
+++
@@ -1,4 +0,0 @@
-export { POSTS_QUERY } from "./queries";
-export { USERS_QUERY } from "./queries";
-export { CREATE_POST_MUTATION } from "./mutations";
-export { POSTS_SUBSCRIPTION } from "./subscriptions";
